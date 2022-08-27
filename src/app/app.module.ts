fd9e382dd11d03cd39c20424ac76e1e4f06a3b73
Nicolás Bautista <nick.bau2015@gmail.com>
2022-08-26 23:34:11 -0300

    maquetado inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutComponent } from './components/about/about.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { AttitudesComponent } from './components/attitudes/attitudes.component';
+import { StudiesComponent } from './components/studies/studies.component';
+import { AchievementsComponent } from './components/achievements/achievements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    IntroComponent,
+    ExperienceComponent,
+    AttitudesComponent,
+    StudiesComponent,
+    AchievementsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/attitudes/attitudes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attitudes',
+  templateUrl: './attitudes.component.html',
+  styleUrls: ['./attitudes.component.css']
+})
+export class AttitudesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
