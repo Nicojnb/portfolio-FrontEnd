f3ca884867d80c5d91fa83cc63c6290e5ad793c5
Nicolás Bautista <nick.bau2015@gmail.com>
2022-10-19 00:06:03 -0300

    Auto-generated commit [ci skip]

--- assets/data/Achievements.ts
+++ assets/data/Achievements.ts
@@ -5,12 +5,14 @@ export const ACHIEVEMENTS: IAchievements[] =[
         id: 1,
         name: "FrontEnd para el cv digital",
         description: "Realizado en Typescript con Angular, Nodejs",
-        url: "https://github.com/Nicojnb/portfolio-FrontEnd"
+        url: "https://github.com/Nicojnb/portfolio-FrontEnd",
+        userId: 5456464
     },
     {
         id: 2,
         name: "BackEnd para el cv digital",
         description: "Realizado en JAVA con SpringBoot, MySQL",
-        url: "https://github.com/Nicojnb/portfolio-BackEnd"
+        url: "https://github.com/Nicojnb/portfolio-BackEnd",
+        userId: 5456464
     }
 ]
